b6b0c9c1a9f254ad2c2b256309068b471d230ff9
adzguy <dnurmeto@neiu.edu>
2019-11-26 00:59:52 -0600

    Initial Commit

---
+++ RockPaperScissors/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RockPaperScissors
+//
+//  Created by Davron on 11/26/19.
+//  Copyright © 2019 Davron. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
